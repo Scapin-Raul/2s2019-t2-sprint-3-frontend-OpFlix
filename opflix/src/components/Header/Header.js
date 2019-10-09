7d8fd67c3342056b08ab6ff44f4857e0bd024248
Raul Scapin Do Nascimento <46791749865@info.local>
2019-10-09 11:27:58 -0300

    wire frame alta fidelidade

---
+++ opflix/src/components/Header/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Header(){
+    return(
+        <header>
+            <nav>
+                <a href="Home.html"><img src="https://fontmeme.com/permalink/191008/c1530dbd18ec49e01ec09eda253b5ac4.png" alt="netflix-font" border="0"/></a>
+                <a href="Login.html">Login</a>
+            </nav>
+            <h1 style="display: none">OPFLIX</h1>
+            <h2>Confira os últimos lançamentos do mundo cinematográfico!</h2>
+        </header>
+    );
+}
+
+export default Header;
